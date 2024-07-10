576783c483315a1f5dc25234f3274190506cbf90
karim2002-debug <karimtawfee@gmail.com>
2024-07-10 20:09:17 +0300

    API update

--- World News/Network/APICaller.swift
+++ World News/Network/APICaller.swift
@@ -11,12 +11,12 @@ import Alamofire
 
 final class APICaller{
     struct Constant{
-        static let API_Key = "pub_115299bf6140713371ee13ed9d4bd1b84099b"
+        static let API_Key = "pub_11529a6414f54929aafd7a4de17988c11201d"
         static var articalTitle : [String]!
     }
     static func getNews(completationHandler :@escaping( Result<[Results],Error>)->Void){
         
-        guard let url = URL(string: "https://newsdata.io/api/1/latest?apikey=pub_11529a6414f54929aafd7a4de17988c11201d&q=pegasus&language=en")else{
+        guard let url = URL(string: "https://newsdata.io/api/1/news?apikey=\(APICaller.Constant.API_Key)")else{
             return
         }
         AF.request(url,method: .get).responseDecodable(of: NewsModel.self) { response in
@@ -34,7 +34,7 @@ final class APICaller{
     
     static func getBreakingNews(completationHandler :@escaping( Result<[Results],Error>)->Void){
         
-        guard let url = URL(string: "https://newsdata.io/api/1/latest?apikey=pub_11529a6414f54929aafd7a4de17988c11201d&q=pegasus&language=en")else{
+        guard let url = URL(string: "https://newsdata.io/api/1/latest?apikey=\(APICaller.Constant.API_Key)&q=pegasus&language=en")else{
             return
         }
         AF.request(url,method: .get).responseDecodable(of: NewsModel.self) { response in
@@ -51,7 +51,7 @@ final class APICaller{
     }
     static func getsearchNews(completationHandler :@escaping( Result<[Results],Error>)->Void){
         
-        guard let url = URL(string: "https://newsdata.io/api/1/news?apikey=pub_11529a6414f54929aafd7a4de17988c11201d")else{
+        guard let url = URL(string: "https://newsdata.io/api/1/news?apikey=\(APICaller.Constant.API_Key)")else{
             return
         }
         AF.request(url,method: .get).responseDecodable(of: NewsModel.self) { response in
@@ -69,7 +69,7 @@ final class APICaller{
     static func searchForNews(query : String , completionHandler : @escaping(Result<[Results],Error>)->Void){
         guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)else{return}
 
-        guard let url = URL(string: "https://newsdata.io/api/1/news?apikey=pub_11529a6414f54929aafd7a4de17988c11201d&q=\(query)")else{
+        guard let url = URL(string: "https://newsdata.io/api/1/news?apikey=\(APICaller.Constant.API_Key)&q=\(query)")else{
             return
         }
         AF.request(url,method: .get).responseDecodable(of: NewsModel.self) { reponse  in
